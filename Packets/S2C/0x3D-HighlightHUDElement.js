f4cbb480ab54104c7673a90dce717be07b688e7e
DantestyleXD <null-fox@hotmail.it>
2021-11-10 15:58:53 +0100

    Revert "Solved merge conflicts"

--- Packets/S2C/0x3D-HighlightHUDElement.js
+++ Packets/S2C/0x3D-HighlightHUDElement.js
@@ -1,10 +1,11 @@
 var BasePacket = require('../BasePacket');
 
-module.exports = class extends BasePacket {//S2C.
+
+module.exports = class extends BasePacket {//S2C.HighlightHUDElement
 	struct = {
 		ElementPart: 'uint8',
 		ElementType: 'uint8',
 		ElementNumber: 'uint8',
-		ElementSubCategory: 'uint8',
+		ElementSubCategory : 'uint8',
 	}
 };
